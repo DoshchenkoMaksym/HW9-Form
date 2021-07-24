f01304ef63f26faf5c74fe96e986fa2662bb11d6
Maksym Doshchenko <doschenko54@gmail.com>
2021-07-24 21:14:59 +0300

    fix

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ inputName.addEventListener('input', () => {
         inputName.classList.remove('invalid')
     } 
 });
+
 inputPassword.addEventListener('input', () => {
     if (!correctPass.test(inputPassword.value)) {
         inputPassword.classList.add('invalid')
@@ -20,11 +21,19 @@ inputPassword.addEventListener('input', () => {
         inputPassword.classList.remove('invalid')
     } 
 });
+
 buttonSubmit.addEventListener("click", (event) => {
     event.preventDefault();
     if(correctName.test(inputName.value) && correctPass.test(inputPassword.value)){
         console.log(inputName.value);
         getForm.reset();
-    } 
+    } else if (!correctName.test(inputName.value) && !correctPass.test(inputPassword.value)) {
+        inputName.classList.add('invalid');
+        inputPassword.classList.add('invalid');
+    } else if (!correctName.test(inputName.value)) {
+        inputName.classList.add('invalid');
+    } else if (!correctPass.test(inputPassword.value)) {
+        inputPassword.classList.add('invalid');
+    }
 });
 
